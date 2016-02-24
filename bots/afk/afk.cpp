d471b2d2282615f92e7c0b88df7dfd72faca0d45
Millian Poquet <millian.poquet@inria.fr>
2016-02-24 20:58:26 +0100

    AFK bot updated: readibility of what happens

--- bots/afk/afk.cpp
+++ bots/afk/afk.cpp
@@ -16,21 +16,31 @@ int main(int argc, char ** argv)
     string address = argv[1];
     string port_str = argv[2];
     int port = std::stoi(port_str);
+    string name = "AFK";
 
     try
     {
         Session session;
-        session.connect(address, port);
-        session.login_player("AFK");
 
+        printf("Connecting to %s:%d...\n", address.c_str(), port);
+        session.connect(address, port);
+
+        printf("Logging in as a player named '%s'...\n", name.c_str());
+        session.login_player(name);
+
+        printf("Waiting for WELCOME...\n");
         session.wait_for_welcome();
+
+        printf("Waiting for GAME_STARTS...\n");
         session.wait_for_game_starts();
 
         while(session.is_logged())
         {
             Actions actions;
+            printf("Sending actions...\n");
             session.send_actions(actions);
 
+            printf("Waiting for next turn...\n");
             session.wait_for_next_turn();
         }
     }
